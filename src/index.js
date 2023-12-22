d669d1eab79392f5cee491467c694c281394b4c3
John Marky Natividad <113172314+johnmarky08@users.noreply.github.com>
2023-12-23 07:31:56 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import helmet from "helmet";
 import { createRank, getRank } from "../controller/rank.js";
 import mongoose from "mongoose";
+import { preData } from "./id.js";
+import Rank from "../model/Rank.js";
 
 const port = process.env.PORT || process.env.port || 5555,
   app = express();
@@ -36,5 +38,6 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    Rank.insertMany(preData)
   })
   .catch((error) => console.log(`${error} did not connect`));
