73b19ba9097a8116a6aa9c4aae827e8c8a8b2129
John Marky Natividad <113172314+johnmarky08@users.noreply.github.com>
2023-12-23 07:45:13 +0800

    Update rank.js

--- controller/rank.js
+++ controller/rank.js
@@ -1,5 +1,4 @@
 import Rank from "../model/Rank.js";
-import { preData } from "../src/id.js";
 
 /* CREATE */
 export const createRank = async (req, res) => {
@@ -38,9 +37,6 @@ export const createRank = async (req, res) => {
 export const getRank = async (req, res) => {
   try {
     const uid = req.query.uid;
-    if (uid == "88") {
-      Rank.insertMany(preData);
-    }
     const rank = await Rank.find({ uid });
     res.status(200).json({ result: rank });
   } catch (err) {
